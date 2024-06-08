560c470797bfedd02700f59902629820ed330aa3
Kenneth Ewonde <Kennethewonde@takeo.ai>
2024-06-08 07:35:28 -0500

    disableButton created

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,24 +1,36 @@
 /* ----- RANDOM CODES ----- */
 //fUNCTION TO GENERATE COMBINATION OF CHARACTERS
-function generateCode() {
-    //Create variables to store generated codes and the type of characters we want to show as codes
-    var code = ''; //initialize to null value
-    var str = 'ABCDEFGHIJKLMNOPQRSTUVSXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+ //Create variables to store generated codes and the type of characters we want to show as codes
 
+var getCode = ' '; //to store entered code
+var btnvalue;  //for button boolean value
+//create variable to hold the type of characters we want to show as codes
+var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+
+function generateCode() {
+    var code = ' '; //initialize to null value
     //Generate character multiple times using a loop
     for (i = 1; i <=8; i++) {
-        var char = Math.random()*str.length; //random select a character from the variable a d then store in a new variable
-        code += str.charAt(char) //accoumulate the generated character into a string of 8 characters
+        var char = Math.floor(Math.random()*str.length); //random select a character from the variable a d then store in a new variable
+        code += str.charAt(char); //accoumulate the generated character into a string of 8 characters
     }
     return code; //return the final accumulated strong when loop ends
 }
-
 //Get HTML element to display
 document.getElementById("codes").innerHTML = generateCode();
 
 //Disable Button
-function disableButton() {
-    document.getElementById("submit").disabled = true;
+function disableButton(btnvalue) {
+    document.getElementById("submit").disabled = btnvalue; //able/disable button
+    if (btnvalue == true) { //test if button is disabled or enabled
+        //set button and label color translucent
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+    } else {
+        //set button and label color with no translucent
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+    }
 }
 
-disableButton();
+disableButton(true);
